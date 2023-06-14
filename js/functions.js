9530883cae95aa05a60984b373951c0a59a79875
dYAROSLAVy <yadsp@yandex.ru>
2023-06-14 19:05:29 +0300

    Исправляет функции после проверки

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,29 @@
 // Функция для проверки длины строки
-const stringLength = (string, maxLength) => string.length <= maxLength;
-stringLength('проверяемая строка', 20);
-stringLength('проверяемая строка', 18);
-stringLength('проверяемая строка', 10);
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+// console.log(stringLength('проверяемая строка', 20));
+// console.log(stringLength('проверяемая строка', 18));
+// console.log(stringLength('проверяемая строка', 10));
 
 // Функция для проверки, является ли строка палиндромом
-function isPalindrome(string) {
-  const normalString = string.replaceAll(' ', '').toLowerCase();
-  let newString = '';
-  for (let i = normalString.length - 1; i >= 0; i--) {
-    newString += normalString[i];
-  }
-  return normalString === newString;
-}
-isPalindrome('топот');
-isPalindrome('ДовОд');
-isPalindrome('Кекс');
+const isPalindrome = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+  return string === string.split('').reverse('').join('');
+};
+// console.log(isPalindrome('топот'));
+// console.log(isPalindrome('ДовОд'));
+// console.log(isPalindrome('Кекс'));
+// console.log(isPalindrome('Лёша на полке клопа нашёл '));
 
 // Функция для извлечения чисел
-function returnNumbers(string) {
-  string = string.toString();
-  let newString = '';
-  for (let i = 0; i <= string.length - 1; i++) {
-    const number = parseInt(string[i], 10);
-    if (!Number.isNaN(number)) {
-      newString += number;
-    }
-  }
-  return parseInt(newString, 10);
-}
-returnNumbers('2023 год');
-returnNumbers('ECMAScript 2022');
-returnNumbers('1 кефир, 0.5 батона');
-returnNumbers('агент 007');
-returnNumbers('а я томат');
-returnNumbers(2023);
-returnNumbers(-1);
-returnNumbers(1.5);
+const returnNumbers = (string) => {
+  string = String(string) .replace(/\D/g, '');
+  return parseInt(string, 10);
+};
+// console.log(returnNumbers('2023 год'));
+// console.log(returnNumbers('ECMAScript 2022'));
+// console.log(returnNumbers('1 кефир, 0.5 батона'));
+// console.log(returnNumbers('агент 007'));
+// console.log(returnNumbers('а я томат'));
+// console.log(returnNumbers(2023));
+// console.log(returnNumbers(-1));
+// console.log(returnNumbers(1.5));

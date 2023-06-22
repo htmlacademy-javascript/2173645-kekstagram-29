b1fb88af40d2c97c87ecf94f7c41908367908f99
dYAROSLAVy <yadsp@yandex.ru>
2023-06-22 23:36:30 +0300

    Добавляет модули

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
-checkStringLength();
+checkStringLength(10, 10);
 
 const isPalindrome = (string) => {
   string = string.replaceAll(' ', '').toLowerCase();
   return string === string.split('').reverse('').join('');
 };
-isPalindrome();
+isPalindrome('иди');
 
 const returnNumbers = (string) => {
   string = String(string) .replace(/\D/g, '');
   return parseInt(string, 10);
 };
-returnNumbers();
+returnNumbers('dffd10');
